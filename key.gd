715eb077e96dd50dd1250c8b55fc7f7c0ab54f17
Daniel Mau <daniel.b.mau@gmail.com>
2024-10-29 09:43:38 -0400

    Added multiple choice keys

--- key.gd
+++ key.gd
@@ -9,6 +9,9 @@ var key_tooth_3_scene_instance
 var key_tooth_1_marker
 var key_tooth_2_marker
 var key_tooth_3_marker
+var rng 
+var ready_positions
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,9 +22,12 @@ func _ready() -> void:
 	key_tooth_2_marker = get_node("KeyTooth2Marker")
 	key_tooth_3_marker = get_node("KeyTooth3Marker")
 	
+	rng = RandomNumberGenerator.new()
+	ready_positions = true
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Global.pin_line_position_1 && Global.pin_line_position_2 && Global.pin_line_position_3:
+	if ready_positions == true && Global.pin_line_position_1 && Global.pin_line_position_2 && Global.pin_line_position_3:
 		set_positions()
 
 func set_positions():
@@ -29,14 +35,15 @@ func set_positions():
 	key_tooth_2_scene_instance = key_tooth_2_scene.instantiate()
 	key_tooth_3_scene_instance = key_tooth_3_scene.instantiate()
 	
-	#key_tooth_1_scene_instance.position.y += Global.pin_line_position_1 * 100
-	#key_tooth_2_scene_instance.position.y += Global.pin_line_position_2 * 100
-	#key_tooth_3_scene_instance.position.y += Global.pin_line_position_3 * 100
-	
-	key_tooth_1_scene_instance.position.y += Global.pin_line_position_1 * -100
-	key_tooth_2_scene_instance.position.y += Global.pin_line_position_2 * -100
-	key_tooth_3_scene_instance.position.y += Global.pin_line_position_3 * -100
-	
+	if self.name == "Key":
+		key_tooth_1_scene_instance.position.y += Global.pin_line_position_1 * -100
+		key_tooth_2_scene_instance.position.y += Global.pin_line_position_2 * -100
+		key_tooth_3_scene_instance.position.y += Global.pin_line_position_3 * -100
+	else:
+		key_tooth_1_scene_instance.position.y += rng.randf_range(0.0, 0.5) * -100
+		key_tooth_2_scene_instance.position.y += rng.randf_range(0.0, 0.5) * -100
+		key_tooth_3_scene_instance.position.y += rng.randf_range(0.0, 0.5) * -100
+		
 	key_tooth_1_scene_instance.position.x = key_tooth_1_marker.position.x
 	key_tooth_2_scene_instance.position.x = key_tooth_2_marker.position.x
 	key_tooth_3_scene_instance.position.x = key_tooth_3_marker.position.x
@@ -44,3 +51,5 @@ func set_positions():
 	add_child(key_tooth_1_scene_instance)
 	add_child(key_tooth_2_scene_instance)
 	add_child(key_tooth_3_scene_instance)
+	
+	ready_positions = false
